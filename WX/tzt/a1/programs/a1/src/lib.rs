d8ea4dd88dde9dcaf6d42d5716348e1d78da340c
kachraz <110726496+kachraz@users.noreply.github.com>
2025-08-31 13:15:24 +0000

    🫦

--- WX/tzt/a1/programs/a1/src/lib.rs
+++ WX/tzt/a1/programs/a1/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5cArbGqLdr1T6FPZFqb6eDKAHjUMqEzjKDkqSBuLc1br");
+declare_id!("H1g8cXJBZqv6H2PZw8Hakq3qi6MBBmAUNqmUx3sYH4CW");
 
 #[program]
 pub mod a1 {
